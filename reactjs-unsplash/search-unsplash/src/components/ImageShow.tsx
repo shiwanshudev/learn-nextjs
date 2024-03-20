9b5076285689a3e7898627b16d821281a5547d3c
Shiwanshu Shubham <shiwanshudev@protonmail.com>
2024-03-20 23:05:50 +0530

    Add basic styling to form

--- reactjs-unsplash/search-unsplash/src/components/ImageShow.tsx
+++ reactjs-unsplash/search-unsplash/src/components/ImageShow.tsx
@@ -1,12 +1,12 @@
 export default function ImageShow({ alt, src }: { alt: string; src: string }) {
   return (
-    <div>
-      <p style={{ textTransform: "capitalize" }}>{alt}</p>
+    <div style={{ width: "350px" }}>
       <img
         src={src}
         alt={alt}
         style={{ width: "350px", height: "350px", objectFit: "cover" }}
       />
+      <p style={{ textTransform: "capitalize", fontSize: "0.90rem" }}>{alt}</p>
     </div>
   );
 }
--- reactjs-unsplash/search-unsplash/src/components/SearchImage.tsx
+++ reactjs-unsplash/search-unsplash/src/components/SearchImage.tsx
@@ -7,13 +7,46 @@ export default function SearchImage({ onSubmit }: { onSubmit: any }) {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form
+      onSubmit={handleSubmit}
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        marginBottom: "20px",
+      }}
+    >
       <input
         type="text"
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
+        style={{
+          padding: "10px",
+          marginRight: "5px",
+          width: "57%",
+          background: "#eee",
+          outline: "none",
+          border: "none",
+          borderRadius: "5px",
+          height: "45px",
+          boxSizing: "border-box",
+          fontSize: "1.1rem",
+        }}
+        placeholder="Search Unsplash Here..."
       />
-      <button>Search</button>
+      <button
+        style={{
+          backgroundColor: "black",
+          color: "white",
+          padding: "10px",
+          fontSize: "1.1rem",
+          border: "none",
+          borderRadius: "5px",
+          cursor: "pointer",
+          height: "45px",
+        }}
+      >
+        Search
+      </button>
     </form>
   );
 }

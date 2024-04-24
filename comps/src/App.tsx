beb0c0a5215a4b1c69561736920ce267211c1d94
shiwanshudev <shiwanshudev@protonmail.com>
2024-04-24 22:27:02 +0530

    Add tailwindcss rules for buttons

--- comps/src/App.tsx
+++ comps/src/App.tsx
@@ -1,4 +1,22 @@
 import Button from "./Button";
 export default function App() {
-  return <Button primary>Hey</Button>;
+  return (
+    <div className=" flex items-center justify-center">
+      <Button primary rounded outline>
+        Primary
+      </Button>
+      <Button secondary rounded>
+        Secondary
+      </Button>
+      <Button warning rounded>
+        Secondary
+      </Button>
+      <Button success rounded>
+        Secondary
+      </Button>
+      <Button danger rounded>
+        Secondary
+      </Button>
+    </div>
+  );
 }
--- comps/src/Button.tsx
+++ comps/src/Button.tsx
@@ -1,3 +1,5 @@
+import clsx from "clsx";
+
 interface ButtonProps {
   children: string;
   primary?: boolean | undefined;
@@ -5,22 +7,38 @@ interface ButtonProps {
   warning?: boolean | undefined;
   success?: boolean | undefined;
   danger?: boolean | undefined;
-  outline?: boolean | undefined;
   rounded?: boolean | undefined;
+  outline?: boolean | undefined;
+  className?: string | undefined;
 }
 
 export default function Button({
   children,
-  outline,
   rounded,
+  outline,
+  className,
   ...props
 }: ButtonProps) {
   const variants = Object.keys(props);
   if (variants.length > 1) {
     throw new Error(
-      "Only one of primary, secondary, warning, success danger can be true!"
+      "Only one of primary, secondary, warning, success or danger can be true!"
     );
   }
+  const { primary, secondary, warning, success, danger } = props;
+  const classes = clsx("px-3 py-2 cursor-pointer", {
+    "bg-blue-700  text-white": primary,
+    "border border-zinc-300": secondary,
+    "bg-yellow-500 text-white": warning,
+    "bg-green-500 text-white": success,
+    "bg-red-500 text-white": danger,
+    "rounded-lg": rounded,
+    "text-blue-700 border border-blue-700 bg-transparent": outline && primary,
+    "text-blue-700 border border-zinc-300 bg-transparent": outline && secondary,
+    "text-blue-700 border border-yellow-500 bg-transparent": outline && warning,
+    "text-blue-700 border border-green-500 bg-transparent": outline && success,
+    "text-blue-700 border border-red-500 bg-transparent": outline && danger,
+  });
 
-  return <button>{children}</button>;
+  return <button className={`${classes} ${className}`}>{children}</button>;
 }

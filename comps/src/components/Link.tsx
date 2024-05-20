7d55bf571afbd1aa8b27e362b90c99e479f1ad76
shiwanshudev <shiwanshudev@protonmail.com>
2024-05-20 11:08:52 +0530

    Add ctrl click behavior to link

--- comps/src/components/Link.tsx
+++ comps/src/components/Link.tsx
@@ -4,9 +4,16 @@ export default function Link({ to, children }: { children: any; to: string }) {
   const { navigate } = useContext(NavigationContext);
 
   const handleClick = (e: React.SyntheticEvent) => {
+    if (e.ctrlKey || e.metaKey) {
+      return;
+    }
     e.preventDefault();
     navigate(to);
   };
 
-  return <a onClick={handleClick}>{children}</a>;
+  return (
+    <a href={to} onClick={handleClick}>
+      {children}
+    </a>
+  );
 }
